3bb0a55f890d8399d8cd5b8187e61074ac041f18
Jada Racquel <mccoyjada.9816@gmail.com>
2020-05-21 23:14:22 -0500

    Implemented Animal class and tested it in Test class

--- OOPPractice/Animal.cs
+++ OOPPractice/Animal.cs
@@ -6,7 +6,82 @@ using System.Threading.Tasks;
 
 namespace OOPPractice
 {
+    //***EXAMPLE OF INHERITANCE & POLYMORPHISM***      KEYWORDS: virtual- for base class methods; override- for derived class methods
     class Animal
     {
+        public virtual void Eat()
+        {
+            Console.WriteLine("Eating like any other animal...");
+        }
+        public virtual void Walk()
+        {
+            Console.WriteLine("Walking like any other animal...");
+        }
+        public virtual void Sleep()
+        {
+            Console.WriteLine("Sleeping like any other animal...");
+        }
+
+    }
+    //A Lion 'IS-A' Animal that can eat, walk, sleep, and attack
+    class Lion : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eating like a lion...");
+        }
+        public override void Walk()
+        {
+            Console.WriteLine("Walking like a lion...");
+        }
+        public override void Sleep()
+        {
+            base.Sleep();
+        }
+        public void Attack()
+        {
+            Console.WriteLine("Attacking like a lion!!!");
+        }
+    }
+    //A Giraffe 'IS-A' Animal that can eat, walk, sleep, and look around
+    class Giraffe : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eating like a giraffe...");
+        }
+        public override void Walk()
+        {
+            base.Walk();
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleeping like a giraffe...");
+        }
+        public void LookAround()
+        {
+            Console.WriteLine("Looking around like a giraffe!!!");
+        }
+    }
+    //A Duck 'IS-A' Animal that can eat, walk, sleep, and swim
+    class Duck : Animal
+    {
+        public override void Eat()
+        {
+            base.Eat();
+        }
+        public override void Walk()
+        {
+            Console.WriteLine("Walking like a duck...");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleeping like a duck...");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Swimming like a duck!!!");
+        }
     }
 }
+
--- OOPPractice/Test.cs
+++ OOPPractice/Test.cs
@@ -8,6 +8,22 @@ namespace OOPPractice
 {
     class Test
     {
+        public static void FeedAnimals(Animal[] theZoo)
+        {
+            //Polymorphism-- this FeedAnimal() method will feed any type of animal
+            for (int i = 0; i < theZoo.Length; i++)
+            {
+                theZoo[i].Eat();
+            }
+        }
+        public static void WalkAnimals(Animal[] theZoo)
+        {
+            //Polymorphism-- this WalkAnimal() method will walk any type of animal
+            for (int i = 0; i < theZoo.Length; i++)
+            {
+                theZoo[i].Walk();
+            }
+        }
         static void Main(string[] args)
         {
             //***TEST STUDENT CLASS***
@@ -24,6 +40,28 @@ namespace OOPPractice
             Console.WriteLine("Student ID: {0}\n", James.GetID());
 
             Console.WriteLine(Wilson.ToString() + "\n");
+
+            //***TEST ANIMAL CLASS***
+            //Lion Leon = new Animal();     //This DOES NOT work b/c every animal is NOT a lion!
+            Animal Leo = new Lion();
+            Animal Dezz = new Duck();   //This works b/c every lion is an animal!
+            Animal Gia = new Giraffe();
+            Duck Daffy = new Duck();
+            Lion Lava = new Lion();
+            Giraffe Gina = new Giraffe();
+            Duck Dula = new Duck();
+            //The line below works b/c the Lion, Giraffe, and Duck classes all inherit the Animal class 
+            Animal[] animals = new Animal[] { Leo, Dezz, Lava, Gia, Dula, Gina, Daffy };
+
+            FeedAnimals(animals);
+            Console.WriteLine();
+
+            WalkAnimals(animals);
+            Console.WriteLine();
+
+            Gina.Sleep();
+            Dezz.Sleep();
+            Lava.Sleep();
         }
     }
 }
