82428b73238a5614222eca40f37985b711d9b636
Jada Racquel <mccoyjada.9816@gmail.com>
2020-05-22 01:43:53 -0500

    Implemented Human class; Added derived Female and Male classes

--- OOPPractice/Human.cs
+++ OOPPractice/Human.cs
@@ -6,8 +6,80 @@ using System.Threading.Tasks;
 
 namespace OOPPractice
 {
-    class Human
+    //***EXAMPLE OF ABSTRACTION & INHERITANCE***
+    public abstract class Human
     {
+        public string name;
+        public int age;
+        public Human()
+        {
+            Console.WriteLine("Enter your name: ");
+            string humanName = Console.ReadLine();
+            this.name = humanName;
+            Console.WriteLine("Enter your age: ");
+            int humanAge = int.Parse(Console.ReadLine());
+            this.age = humanAge;
+        }
+        public Human(string humanName)
+        {
+            this.name = humanName;
+        }
+
+        public Human(string humanName, int humanAge)
+        {
+            this.name = humanName;
+            this.age = humanAge;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Eating like a human...");
+        }
+
+        public virtual void Shave()
+        {
+            Console.WriteLine("Shaving like a human...");
+        }
+
+        public abstract void Work(string career);
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping like a human...");
+        }
+    }
+
+    public class Female : Human
+    {
+        public int femaleAge;
+        public Female(string nm, int age) : base(nm)
+        {
+            femaleAge = age;
+        }
+        public override void Shave()
+        {
+            Console.WriteLine("Shaving like a female...");
+        }
+        public override void Work(string job)
+        {
+            Console.WriteLine("Female working as a " + job);
+        }
 
     }
+    public class Male : Human
+    {
+        public Male(string nm, int maleAge) : base(nm, maleAge)
+        {
+
+        }
+        public override void Shave()
+        {
+            base.Shave();
+        }
+        public override void Work(string job)
+        {
+            Console.WriteLine("Male working as a " + job);
+        }
+
+    }
+
 }
