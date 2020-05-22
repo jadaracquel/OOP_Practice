f51a2a344ffa049385659ee92d82f6f71fd6ffe0
Jada Racquel <mccoyjada.9816@gmail.com>
2020-05-21 22:53:19 -0500

    Added Student and Test classes

---
+++ OOPPractice/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    //EXAMPLE OF ENCAPSULATION
+    class Student
+    {
+        private string name;
+        private string id;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetID()
+        {
+            return id;
+        }
+
+        public void SetID(string id)
+        {
+            this.id = id;
+        }
+
+        public override String ToString()
+        {
+            return "Student Name: " + GetName() + "\nStudent ID: " + GetID();
+        }
+    }
+}
---
+++ OOPPractice/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //***TEST STUDENT CLASS***
+            Student James = new Student();
+            Student Wilson = new Student();
+
+            James.SetName("Armani James");
+            Wilson.SetName("Keith Wilson");
+
+            Wilson.SetID("36851");
+            James.SetID("5274");
+
+            Console.WriteLine("Student Name: {0}", James.GetName());
+            Console.WriteLine("Student ID: {0}\n", James.GetID());
+
+            Console.WriteLine(Wilson.ToString() + "\n");
+        }
+    }
+}
